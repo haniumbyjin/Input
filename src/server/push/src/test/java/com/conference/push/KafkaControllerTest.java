e207a4e341336f72ada22e988d40eba273990d48
LeeYunJae <yunjea0312@naver.com>
2019-07-01 14:24:35 +0900

    Change: kafka test code

--- src/server/push/src/test/java/com/conference/push/KafkaControllerTest.java
+++ src/server/push/src/test/java/com/conference/push/KafkaControllerTest.java
@@ -6,6 +6,7 @@ import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 
@@ -22,12 +23,15 @@ public class KafkaControllerTest {
     @Autowired
     private MockMvc mockMvc;
 
-//    @Test
-//    public void kafkaTest() throws Exception {
-//        Message message = new Message();
-//        message.setMessage("helloWorld");
-//        mockMvc.perform(post("/kafka/test", message))
-//                .andExpect(status().isOk())
-//                .andDo(print());
-//    }
+    @Test
+    public void kafkaTest() throws Exception {
+
+        mockMvc.perform(post("/kafka/test")
+                .content("{\n" +
+                        "\t\"message\" : \"hi\"\n" +
+                        "}")
+                .contentType(MediaType.APPLICATION_JSON_UTF8)) // content의 type을 명시합니다.
+                .andExpect(status().isOk())
+                .andDo(print());
+    }
 }
