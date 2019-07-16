62079b4f259565fcbc9f0107f8761114cfc505ab
seyoung <airseyoung@naver.com>
2019-07-16 19:03:36 +0900

    Change: File/init

--- src/client/src/component/FileUpload.js
+++ src/client/src/component/FileUpload.js
@@ -1,16 +1,29 @@
 import React, {Component} from 'react';
+import axios from 'axios';
 
 class Fileupload extends Component{
+    state = {
+        selectedFile : null
+    }
 
     fileSelectedHandler = event =>{
         // 파일선택
-    }
+        this.setState =({
+            selectedFile: event.target.files[0]
+        })
+    } 
 
     fileUploadHandler = () =>{
         // 파일 업로드
+        const fd = new FormData();
+        fd.append(this.state.selectedFile, this.state.selectedFile);
+        axios.post('http://localhost:8080/upload', fd)
+        .then(res =>{
+            console.log(res);
+        });
     }
 
-
+ 
     render(){
         return(
             <div>
