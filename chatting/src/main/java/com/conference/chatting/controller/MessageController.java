c53f40cfff3709a4956dd964763751b27b924c04
YunJae Lee <yunjea0312@naver.com>
2019-08-01 19:18:46 +0900

    Add: client-chatting-websocket (#36)

--- chatting/src/main/java/com/conference/chatting/controller/MessageController.java
+++ chatting/src/main/java/com/conference/chatting/controller/MessageController.java
@@ -6,13 +6,24 @@ import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.SendTo;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 @Slf4j
 @RestController
 public class MessageController {
 
     @MessageMapping("/chat")
-    @SendTo("topic/roodId")
+    @SendTo("/topic/roomId")
     public Message broadcasting(Message message) throws Exception{
+        log.info("message = " + message);
+
+        DateFormat sdFormat = new SimpleDateFormat("yyyy-MM-dd hh:mm");
+        Date nowDate = new Date();
+        String date = sdFormat.format(nowDate);
+        message.setTimeStamp(date);
+
         return message;
     }
 }
