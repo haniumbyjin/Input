44774d4b7b2381e4172ed5440df852ec1490bb17
LeeYunJae <yunjea0312@naver.com>
2019-08-19 14:54:15 +0900

    Fix: LocalDate to ISO8601 as UTC

--- chatting/src/main/java/com/conference/chatting/controller/MessageController.java
+++ chatting/src/main/java/com/conference/chatting/controller/MessageController.java
@@ -1,6 +1,8 @@
 package com.conference.chatting.controller;
 
 import com.conference.chatting.model.Message;
+import com.conference.chatting.model.enums.ReqCity;
+import com.conference.chatting.model.enums.ReqNation;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.kafka.core.KafkaTemplate;
@@ -8,7 +10,10 @@ import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.SendTo;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.time.LocalDate;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.TimeZone;
 
 @Slf4j
 @RestController
@@ -26,8 +31,13 @@ public class MessageController {
     @SendTo("/topic/roomId")
     public Message broadcasting(Message message) throws Exception{
 
-        LocalDate currentDate = LocalDate.now();
-        message.setTimeStamp(currentDate.toString());
+//        LocalDate currentDate = LocalDate.now();
+        TimeZone tz = TimeZone.getTimeZone(ReqNation.Asia.getMessage() + "/" + ReqCity.Seoul.getMessage());
+        DateFormat df = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm'Z'"); // Quoted "Z" to indicate UTC, no timezone offset
+        df.setTimeZone(tz);
+        String nowAsISO = df.format(new Date());
+
+        message.setTimeStamp(nowAsISO);
 
         log.info("message = " + message);
 
@@ -35,7 +45,7 @@ public class MessageController {
 
         //kafka로 보내기
         log.info("kafka - send");
-        kafkaTemplate.send("tutorialspoint", message);
+//        kafkaTemplate.send("tutorialspoint", message);
 
 
         return message;
