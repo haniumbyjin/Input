b644f282506e1194f72229c55d5611a4def5737a
seyoung1 <39934875+seyoung1@users.noreply.github.com>
2019-08-27 17:30:56 +0900

    [Client] 파일 업로드 추가 (#45)
    * Add : client file upload
    
    * remove : remove remark

--- src/client/src/App.js
+++ src/client/src/App.js
@@ -5,6 +5,7 @@ import Meet from "./routes/Meet";
 import { observer, inject } from 'mobx-react';
 import {BrowserRouter as Router, Route, Switch} from "react-router-dom";
 import FileUpload from './component/FileUpload';
+import FilePage from './routes/FilePage';
 
 @inject('stores')
 @observer
@@ -20,12 +21,9 @@ class App extends Component {
         mobx 변수 사용 => {testData}<br/><br/>
         <div >
         <Switch>
-        {/* <Route exact path="/" component={Meet}></Route> */}
+        <Route path="/file" component={FilePage}></Route>
         <Route path="/meet" component={Meet}></Route>
         </Switch>
-        </div>
-        <div>
-            <FileUpload/>
         </div>
     </div>
    </Router>
--- src/client/src/component/FileUpload.js
+++ src/client/src/component/FileUpload.js
@@ -2,36 +2,91 @@ import React, {Component} from 'react';
 import axios from 'axios';
 
 class Fileupload extends Component{
-    state = {
-        selectedFile : null
-    }
 
-    fileSelectedHandler = event =>{
-        // 파일선택
-        this.setState =({
-            selectedFile: event.target.files[0]
-        })
-    } 
-
-    fileUploadHandler = () =>{
-        // 파일 업로드
-        const fd = new FormData();
-        fd.append(this.state.selectedFile, this.state.selectedFile);
-        axios.post('http://localhost:8080/upload', fd)
-        .then(res =>{
-            console.log(res);
-        });
-    }
-
- 
-    render(){
-        return(
-            <div>
-                <input type="file" onChange={this.fileSelectedHandler}/>
-                <button onClick={this.fileUploadHandler}>파일 업로드</button>
-            </div>
-        );
+constructor(){
+    super();
+    this.state={
+        currentImage : "",
+        fileName :"",
+        status: false
     }
 }
 
+fileHandler = (e) => {
+    console.log(e.target.files[0]);
+    if(e.target.files !=null || e.target.files[0]!=null){
+        this.setState({currentImage : e.target.files[0]});
+        this.setState({status:true});
+        this.setState({fileName: e.target.files[0].name})
+
+        console.log(this.state);
+
+    if(e.target.files && e.target.files[0]){
+        var reader = new FileReader();
+        reader.onload=(e) =>{
+            document.getElementById("cover").setAttribute('src',e.target.result);
+        }
+        reader.readAsDataURL(e.target.files[0]);
+      }    
+    }else{
+        console.log("파일 없음");
+    }
+}
+
+fileUploadHandler = () => {
+    if(this.state.currentImage ==null){
+
+    }else{
+    const fd = new FormData();
+             fd.append('upload',this.state.currentImage);
+    
+          axios({
+           
+            params: {
+                upload: fd
+            },
+            url : 'http://localhost:8082/upload',
+            method:'GET',
+            data: fd,
+            headers: 
+                {Accept: 'application/json',
+                'Content-Type': `multipart/form-data; boundary=${fd._boundary}`}
+          })
+           .then(function(response){
+                console.log(response.data.message);
+            }).catch(function(response){
+                console.log(response);            
+            })
+        }
+}
+removeImage = () => {
+    this.setState({
+        currentImage:"",
+        fileName: "",
+        status :false,
+    })
+}
+
+render(){
+    return(
+        <div className="App">
+            <input type="file" name="file" onChange={this.fileHandler}/>
+        
+        <button onClick={this.fileUploadHandler} >파일 업로드</button>
+        <div>
+        {
+            this.state.status ?(
+                <div className="image-container">
+                <p className="msg">{this.state.fileName}</p>
+                <button onClick={this.removeImage}>X</button>
+                <img id = "cover" src = ""/>
+                </div>
+            ):(
+                <p>선택한 이미지 없음</p>
+            )
+        }
+        </div>
+        </div>
+)};
+}
 export default Fileupload;
